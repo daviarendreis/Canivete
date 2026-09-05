a0130d1e17f18c262e4f7bdc12bad5287eba001b
daviarendreis10-cpu <daviarendreis10@gmail.com>
2026-09-05 20:04:39 -0300

    VerifyForm Todo

--- Canivete-React/Canivete-React/src/features/todo/hooks/useTodo.ts
+++ Canivete-React/Canivete-React/src/features/todo/hooks/useTodo.ts
@@ -18,19 +18,24 @@ export default function useTodos ( ) {
     useEffect(() => {
         localStorage.setItem('todos', JSON.stringify(todos))
     }, [todos])
+
+    function verifyForm (e: React.FormEvent<HTMLFormElement>) {
+        const formData = new FormData(e.currentTarget)
+        const name = formData.get("name")?.toString() || "To-do"
+        const time = formData.get("time")?.toString() || "12:00"
+        const description = formData.get("description")?.toString() || "Realizar essa tarefa"
+        const priorityRaw = formData.get("priority")?.toString()
+        const priority = (priorityRaw === 'low' ||
+                         priorityRaw === 'medium' ||
+                         priorityRaw === 'high') ? priorityRaw as priority : 'low'
+
+        return {name, time, description, priority}
+    }
     
     function addTodo  (e: React.FormEvent<HTMLFormElement>)  {
             e.preventDefault()
     
-            const formData = new FormData(e.currentTarget)
-    
-            const name = formData.get("name")?.toString() || "To-do"
-            const time = formData.get("time")?.toString() || "12:00"
-            const description = formData.get("description")?.toString() || "Realizar essa tarefa"
-            const priorityRaw = formData.get("priority")?.toString()
-            const priority = (priorityRaw === 'low' ||
-                             priorityRaw === 'medium' ||
-                             priorityRaw === 'high') ? priorityRaw as priority : 'low'
+            const {name, time, description, priority} = verifyForm(e)
     
             e.currentTarget.reset()
     
@@ -55,14 +60,7 @@ export default function useTodos ( ) {
     function editTodo (e: React.FormEvent<HTMLFormElement>, id:number )  {
         e.preventDefault()
 
-        const formData = new FormData(e.currentTarget)
-        const name = formData.get("name")?.toString() || "To-do"
-        const time = formData.get("time")?.toString() || "12:00"
-        const description = formData.get("description")?.toString() || "Realizar essa tarefa"
-        const priorityRaw = formData.get("priority")?.toString()
-        const priority = (priorityRaw === 'low' ||
-                         priorityRaw === 'medium' ||
-                         priorityRaw === 'high') ? priorityRaw as priority : 'low'
+        const {name, time, description, priority} = verifyForm(e)
 
         setTodos((state) => state.map((todo) => todo.id === id ? { ...todo, name, time, description, priority } : todo))
         setEditingTodoId(null)
