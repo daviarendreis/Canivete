5447a110e7ef0a3bb1b79b7b8419c8019bccf9b6
daviarendreis10-cpu <daviarendreis10@gmail.com>
2026-09-10 17:30:26 -0300

    Iniciando Tracker

--- Canivete-React/Canivete-React/src/features/tracker/Tracker.tsx
+++ Canivete-React/Canivete-React/src/features/tracker/Tracker.tsx
@@ -1,10 +1,12 @@
 import { RulerSquareIcon } from "@radix-ui/react-icons";
-import { Box, Heading } from "@radix-ui/themes";
+import { Box, Flex, Heading } from "@radix-ui/themes";
 
 export default function Tracker () {
     return (
         <Box>
-            <Heading color="purple"><RulerSquareIcon/> Tracker</Heading>
+            <Flex>
+                <Heading color="purple"><RulerSquareIcon/> Tracker</Heading>
+            </Flex>
         </Box>
     )
 }
