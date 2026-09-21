77f72e50025f226ef0c631e76b2bf0e8259538cc
daviarendreis10-cpu <daviarendreis10@gmail.com>
2026-09-21 18:18:54 -0300

    NotesCard component

--- src/features/notes/Notes.tsx
+++ src/features/notes/Notes.tsx
@@ -1,8 +1,8 @@
-import { CaretRightIcon, FileTextIcon, MagnifyingGlassIcon, PlusIcon } from "@radix-ui/react-icons";
+import { MagnifyingGlassIcon, PlusIcon } from "@radix-ui/react-icons";
 import { Button, Card, Container, Dialog, Flex, Heading, Table, Text, TextField } from "@radix-ui/themes";
-import { Link } from "react-router-dom";
 import useNotes from "./hooks/useNotes";
 import { useState } from "react";
+import NotesCard from "./components/NotesCard";
 
 export interface Pages {
     id: number,
@@ -70,18 +70,7 @@ export default function Notes () {
                         <Table.Body >
                             { filteredPages.length > 0 ? 
                                 filteredPages.map(page => (
-                                    <Table.Row >
-                                        <Table.Cell>
-                                                <Flex justify={'between'}>
-                                                    <Flex gap={'3'}>
-                                                        <FileTextIcon color="gray" width={'1.5rem'} height={'1.25rem'}/>{page.title}
-                                                    </Flex>
-                                                    <Button color="gray" variant="ghost">
-                                                        <Link to={`/notes/${page.id}`}><CaretRightIcon width={'1.5rem'} height={'1.5rem'}/></Link>
-                                                    </Button>
-                                                </Flex>
-                                        </Table.Cell>
-                                    </Table.Row> ))
+                                    <NotesCard page={page} /> ))
                             : <Text color="gray">There are no pages yet</Text>}
                         </Table.Body>
                     </Table.Root>
---
+++ src/features/notes/components/NotesCard.tsx
@@ -0,0 +1,25 @@
+import { CaretRightIcon, FileTextIcon } from "@radix-ui/react-icons";
+import { Button, Flex, Table } from "@radix-ui/themes";
+import { Link } from "react-router-dom";
+import type { Pages } from "../Notes";
+
+interface NotesCardProps {
+    page: Pages
+}
+
+export default function NotesCard ({page}: NotesCardProps) {
+    return (
+        <Table.Row >
+            <Table.Cell>
+                    <Flex justify={'between'}>
+                        <Flex gap={'3'}>
+                            <FileTextIcon color="gray" width={'1.5rem'} height={'1.25rem'}/>{page.title}
+                        </Flex>
+                        <Button color="gray" variant="ghost">
+                            <Link to={`/notes/${page.id}`}><CaretRightIcon width={'1.5rem'} height={'1.5rem'}/></Link>
+                        </Button>
+                    </Flex>
+            </Table.Cell>
+        </Table.Row>
+    )
+}
